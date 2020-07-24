796cdd402aa4de84426728703eead81041c98439
Jamie Wessels <jamiewessel11@gmail.com>
2020-07-24 14:03:19 -0400

    formatting

--- src/hypo_testing_supermarket.py
+++ src/hypo_testing_supermarket.py
@@ -19,13 +19,14 @@ colors = ['blue', 'red']
 
 overlay_histograms(ax[0], data, labels, colors, xlabel = 'Vehicle Availability (%)', title = 'Vehicle Availability by Supermarket Access')
 ax[0].set_ylabel('Density', fontsize = 14)
+ax[0].set_xlabel('Vehicle Availability (%)', fontsize = 14)
 
 dfs_c = [sprmkt_access, no_sprmkt_access]
 columns_c = ['pct_vehicle_avail', 'pct_vehicle_avail']
 labels_c = ['Supermarket within 0.5 mi', 'No Supermarket within 0.5 mi']
 
 overlay_plots(ax[1], dfs_c, columns_c, labels_c, colors, 'CDF: Poverty Percentage Based on Supermarket Access')
-ax[1].set_xlabel('Vehicle Availability (%)', fontsize = 20)
+ax[1].set_xlabel('Vehicle Availability (%)', fontsize = 14)
 ax[1].set_ylabel('Cumulative Density', fontsize = 14)
 
 # fig.savefig('../images/pdfandcdf_pct_vehicle_by_supermarket.jpeg')
@@ -39,7 +40,7 @@ fig, ax = plt.subplots(figsize = (10, 10))
 num_bs_samps = 5000
 conf_level = 0.95
 plot_bs_sample_diffs(ax, no_sprmkt_access['pct_vehicle_avail'], sprmkt_access['pct_vehicle_avail'], num_bs_samps, np.mean, conf_level, 'Comparison of Mean Vehicle Availability based on Supermarket Access', 'Difference in Bootstrap Sample Means: \n Sample1: Supermarket Access (within 0.5 mi) | Sample 2: No Supermarket Access')
-
+ax.set_ylabel('Density', fontsize = 14)
 fig.tight_layout()
 # fig.savefig('../images/Veh_avail_supermarkets_compare_means.jpeg')
 plt.show()
